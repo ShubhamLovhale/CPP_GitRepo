2f47920c0d298d8d56cc044837b600fd96dcafa3
ShubhamLovhale <sblovhale@gmail.com>
2024-04-14 19:15:59 +0530

    Calculator using switch statement in CPP

---
+++ CPP_SwitchStatement/CPP_SwitchStatement.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+void Calculator(int x, int y)  // Using Switch case or switch statements
+{
+	char Operation = NULL;  // +, -, /, *
+	
+
+	cout << "Please Enter the operation type from +, -, /, *" << endl;
+	cin >> Operation;
+
+	bool isOperationSuitable = false; // Boolean variable 
+
+	if (Operation == '+' || Operation == '-' || Operation == '/' || Operation == '*')
+	{
+		isOperationSuitable = true;
+	}
+
+	else
+	{
+		cout << "Invalid operator value entered!" << endl;
+	   
+	}
+
+
+
+	if (isOperationSuitable)
+	{
+		switch (Operation)
+		{
+			case '+':
+				cout << x + y << endl;
+				break; // break is used to stop the further execution of loop
+
+			case '-':
+				cout << x - y << endl;
+				break;
+
+			case '/':
+				cout << x / y << endl;
+				break;
+
+			case '*':
+				cout << x * y << endl;
+				break;
+
+			default:
+				break;
+
+
+		}
+	}
+
+	
+}
+
+
+
+int main()
+{
+
+	int x = 0;
+	int y = 0;
+	string UserResponse = ""; // THis is string and string is char array.
+	RestartCalculator:    // This is label for goto statement
+	cout << "Starting Calculator" << endl;
+	cout << "please enter 2 numbers" << endl;
+	
+	cin >> x;
+	cin >> y;
+	
+	Calculator(x, y);
+
+	cout << "do you want to use the calculator again ?" << endl;
+	cin >> UserResponse; // yes -- Y
+	//string Converted = ""; // YES
+	
+	//for (int i = 0; i < UserResponse.length(); i++)
+	//{
+	//	char u = UserResponse[i]; // Y
+	//	char 
+	//	Converted[i] = (char)toupper(u);  //Y converting u into uppercase and storing in 'i'th index of converted string
+	//}
+	
+	/*transform(s1.begin(), s1.end(), s1.begin(), ::toupper);*/
+	
+	
+	if (UserResponse == "YES")
+	{
+		goto RestartCalculator; // goto is the jump statement used send controller to particular line define with label:
+	}
+
+	else if (UserResponse == "NO")
+	{
+		cout << "Closing Calculator, Thanks for using our Calculator!" << endl;
+	}
+	
+	else
+	{
+		cout<<"Invalid Input Enterd by user, closing the calculator, please restart calculator to use again" << endl;
+	}
+
+}
