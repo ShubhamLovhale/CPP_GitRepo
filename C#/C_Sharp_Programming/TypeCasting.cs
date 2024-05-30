58ce6bd704177c097381fc6472c5321b13909994
ShubhamLovhale <sblovhale@gmail.com>
2024-05-30 20:33:36 +0530

    Type Casting

---
+++ C#/C_Sharp_Programming/TypeCasting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace C_Sharp_Programming
+{
+    internal class TypeCasting
+    {
+        public static void ImplicitCasting()
+        {
+            // Type Casting : Converting one data type to another
+
+            // Implicit Casting : 
+            // 1. Automatically done by compiller
+            // 2. Small size datatype is stored in relatively large sized datatype
+
+            int x = 15; // 4 bytes
+            Console.WriteLine(x);
+            double d1 = x; // 8 bytes
+            Console.WriteLine(d1);
+
+            // Explicit Casting : 
+            // 1. Has to be done by user
+            // 2. Large size datatype is stored in relatively small sized datatype
+
+            d1 = 25.256;
+            Console.WriteLine(d1);
+            int y = (int)d1;
+            Console.WriteLine(y);
+
+        }
+    }
+}
