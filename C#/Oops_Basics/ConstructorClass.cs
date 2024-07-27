8256c93e022b6e601a4f07d95aa8519ccf9490f0
ShubhamLovhale <sblovhale@gmail.com>
2024-07-27 22:30:21 +0530

    C# oops basics

---
+++ C#/Oops_Basics/ConstructorClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Basics
+{
+    internal class ConstructorClass
+    {
+        string Name;
+        int Age;
+        bool IsMinor;
+
+        // constructor:
+        // Def:
+        // 1. Constructore is the spectial function/method of the class having same name as the class.
+        // 2. Constructor do not have return type
+        // 3. Constructor is invoked automatically when object of the class id created
+        // 4. Types Constructor : 
+        // a. Default Constructor :
+        //    Every class has the defualt constructor, even if we do not declare constructor explicitely.
+        //    Default constructor do not have parameters
+        //    Default constructor initialise not static fields to default values..
+        //    Default constructor will only be created automatically, when there is no other constructor in the class.
+        public ConstructorClass()
+        {
+        }
+
+        // b. Parametric Constructor
+        //    Parametric constructor needs to be declared explicitely
+        //    It has the parameters.
+        //    Used When we need Input from User while creating object
+        //    Paramettric constructor initailises feilds to parameter values
+
+        public ConstructorClass(string name, int age, bool IsMinor)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.IsMinor = IsMinor;
+
+        }
+
+
+        // c. Private Constructor 
+        // d. Static constructor
+        // 
+
+        // Structure for constructor:
+        // AccessModifier Classname(Can have the parameters)
+        //{
+        //}
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name : {Name}, \n Age : {Age}, \n isMinor : {IsMinor}");
+        }
+
+    }
+}
---
+++ C#/Oops_Basics/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConstructorClass c = new ConstructorClass("Prathmesh", 23, false);
+            c.PrintDetails();
+
+            Console.WriteLine("\n\n\n");
+
+            ConstructorClass d = new ConstructorClass();
+            d.PrintDetails();
+
+
+
+            Console.WriteLine("\n\n\n");
+
+            c.PrintDetails();
+
+
+            ConstructorClass e = new ConstructorClass();
+            e.PrintDetails();
+
+            // Object....
+            // Object is the representative of the class
+            // Object is created by calling contructor of the class with "new" keyword.
+            // Object has properties(Data Members) and behaviours
+            // Stucture of creating object
+            // ClassName ObjectName = new Classname()
+
+            //Student.Schoolname = "New English School";
+
+            //Student S1; // (S1- Object) //Instance Object decalaration, Calling the Class Student 
+            //    //Memory is allocated to the Static fields of the class as soon as the class is called
+            //S1 = new Student();
+            //// new keyword allocates space in the memory for non static members of the class.
+            ///
+            Program p = // Instance of the class, 
+                new Program(); // Object of the class //  When we use new keyword with constructor the object is created and memory is allocated
+                               // to non static members of the class. 
+
+            //S1.RollNo = 1;
+            //S1.Standard = 7;
+            //S1.PrintStudentData();
+
+
+            //Student S2 = 
+            //    new Student();
+            //S2.Standard = 8;
+            //S2.RollNo = 2;
+            //S2.PrintStudentData();
+
+            //Student S3 = new Student();
+            //S3.Standard = 8;
+            //S3.RollNo = 3;
+            //S3.PrintStudentData();
+
+            
+            //int age;
+            //string Name;
+
+            //Program p1 = new Program();
+
+            //age = 10;
+            //Name = "fejn";
+
+            //Program p2 = new Program();
+
+            //age = 20;
+            //Name = "fdfbhfejn";
+
+            //Program p3 = new Program();
+
+
+            //age = 30;
+            //Name = "cfenod";
+
+
+            //Cars audi = new Cars();
+            //audi.Wheels = 4;
+            //audi.Color = "black";
+            //audi.pritncardetails();
+            
+
+            //Cars BMW = new Cars();
+            //BMW.pritncardetails();
+            
+
+            //for (int i = 0; i < 1000; i++) 
+            //{                
+            //    new Cars();
+            //}
+
+
+        }
+    }
+
+    public class Cars
+    {
+        public int Wheels = 8;
+        public string Color;
+        public void pritncardetails()
+        {
+            Console.WriteLine($"Wheels = {Wheels} \n cars = {Color}");
+        }
+
+    }
+}
